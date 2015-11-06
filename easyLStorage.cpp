791940e553ba1073a1ebdf33405479f9d946c123
Jared Salazar <jared@techaguru.com>
2015-11-06 19:11:08 +0800

    Major Update
    Instead of replacing characters with ‘\b’; copy the lines after the
    first line, delete the file, recreate the file and write the rest of
    the file.

--- easyLStorage.cpp
+++ easyLStorage.cpp
@@ -96,24 +96,47 @@ boolean easyLStorage::deleteData(){
         return false;
     }
 
-    ///Deleting Part///
-    for(int i = 0; i < _index; i++){
-  // open the file. note that only one file can be open at a time,
-    // so you have to close this one before opening another.
-    DB = SDCARD.open(_FILE, FILE_WRITE);
+    ///Copying///
+    String _DATA = "";
+    DB = SDCARD.open(_FILE); // re-open the file for reading:
+      if (DB) {
+          DB.seek(_index);
+          // read from the file until there's nothing else in it:
+          while (DB.available()) {
+            c = DB.read();
+            if(c != '\b'){
+              _DATA += c;
+            }
+          }
+          // close the file:
+          DB.close();
+      } else {
+          // if the file didn't open, print an error:
+          Serial.println("error opening txt file" );
+      }
+      if(_DATA == ""){
+        Serial.println("No Data..." );
+      }
+    ///Deleting file///
+    Serial.println("Deleting line...");
+    SDCARD.remove(_FILE);
 
-    // if the file opened okay, write to it:
+    ///Re-Create File///
+    //Serial.println("Re-Create file...");
+    DB = SDCARD.open(_FILE,FILE_WRITE);
+
+    ///Writing///
     if (DB) {
-        DB.seek(i);
-        Serial.print(".");
-        DB.print("\b"); //write backspace
+        //Serial.println("Caching....");
+        DB.println(_DATA);
+        //Serial.println(_DATA);
         DB.close(); // close the file:
-        delay(10);
-    }else {
-      Serial.println("error opening txt file" ); // if the file didn't open, print an error:
-      return false;
+        Serial.println("done.");
+    } else {
+        Serial.println("error opening txt file" ); // if the file didn't open, print an error:
     }
-  }
+
+  DB.close();
   Serial.print("Deleting Success");
   return true;
 }
