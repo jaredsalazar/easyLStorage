897776e80d5f1a371364791eab00b73266a233da
jaredSalazar <jared@techaguru.com>
2015-12-21 00:56:39 +0800

    Update

--- easyLStorage.h
+++ easyLStorage.h
@@ -7,10 +7,10 @@ class easyLStorage
 {
 public:
   easyLStorage();
-  boolean start();
-  String getData();
-  boolean deleteData();
-  boolean cacheData(String data);
+  boolean start(String _name);
+  String getData(String _name);
+  boolean deleteData(String _name);
+  boolean cacheData(String data, String _name);
   void data();
 };
 
